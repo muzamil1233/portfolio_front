3175c34e9abffc290d7f65c8e08bb1935955b0bd
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-02-27 22:27:38 +0530

    warning message added in contact section

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -11,42 +11,47 @@ import {
   FaWhatsapp,
 } from "react-icons/fa";
 import * as Yup from "yup";
-import { useFormik } from "formik"
+import { useFormik } from "formik";
+import { TextField } from "@mui/material";
 
-const validationScheme=Yup.object().shape({
-  name:Yup.string().matches(/^[A-Za-z\s]+$/, 'Should only contain letters').required("Enter your name"),
-  email:Yup.string().email("Invalid email address").required("Email is required"),
-  message:Yup.string().required("Enter your message"),
-})
+const validationScheme = Yup.object().shape({
+  name: Yup.string()
+    .matches(/^[A-Za-z\s]+$/, "Should only contain letters")
+    .required("Enter your name"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  message: Yup.string().required("Enter your message"),
+});
 const Footer = () => {
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
-  const formik=useFormik({
-    initialValues:{
-      name:"",
-      email:"",
-      message:"",
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      message: "",
     },
-    validationSchema:validationScheme,
-    onSubmit:(values)=>{
+    validationSchema: validationScheme,
+    onSubmit: (values) => {
       setLoading(true);
       emailjs
-      .send(
-        "service_jcvup4k",
-        "template_9x4idnr",
-        values,
-        "nULCbUGE21ml8lSPw"
-      )
-      .then((result) => {
-        console.log(result.text);
-        setIsFormSubmitted(true);
-      })
-      .catch((error) => {
-        console.error("Email Error:", error);
-        setLoading(false);
-      });
-    }
-  })
+        .send(
+          "service_jcvup4k",
+          "template_9x4idnr",
+          values,
+          "nULCbUGE21ml8lSPw"
+        )
+        .then((result) => {
+          console.log(result.text);
+          setIsFormSubmitted(true);
+        })
+        .catch((error) => {
+          console.error("Email Error:", error);
+          setLoading(false);
+        });
+    },
+  });
 
   return (
     <>
@@ -95,34 +100,41 @@ const Footer = () => {
               name="name"
               value={formik.values.name}
               onChange={formik.handleChange}
-              // error={formik.touched.name && Boolean(formik.errors.name)}
-              // helperText={formik.touched.name && formik.errors.name}
             />
+            {formik.touched.name && formik.errors.name ? (
+              <div className="warning-text">{formik.errors.name}</div>
+            ) : null}
           </div>
           <div className="app__flex">
             <input
               className="p-text"
-              type="email"
-              placeholder="Your Email"
+              type="text"
+              placeholder="Your Email Address"
               name="email"
               value={formik.values.email}
               onChange={formik.handleChange}
-              // error={formik.touched.email && Boolean(formik.errors.email)}
-              // helperText={formik.touched.email && formik.errors.email}
             />
+            {formik.touched.email && formik.errors.email ? (
+              <div className="warning-text">{formik.errors.email}</div>
+            ) : null}
           </div>
-          <div>
+          <div  className="app__flex">
             <textarea
               className="p-text"
               placeholder="Your Message"
               value={formik.values.message}
               name="message"
               onChange={formik.handleChange}
-              // error={formik.touched.message && Boolean(formik.errors.message)}
-              // helperText={formik.touched.message && formik.errors.message}
             />
+            {formik.touched.message && formik.errors.message ? (
+              <div className="warning-text">{formik.errors.message}</div>
+            ) : null}
           </div>
-          <button type="button" className="p-text" onClick={formik.handleSubmit}>
+          <button
+            type="button"
+            className="p-text"
+            onClick={formik.handleSubmit}
+          >
             {!loading ? "Send Message" : "Sending..."}
           </button>
         </div>
